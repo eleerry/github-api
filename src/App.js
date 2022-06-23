d03371dac9bcb79f4a0e1334d95f2a91466b228b
eleerry <luisblack.lr@gmail.com>
2022-06-22 23:32:49 -0300

    Aula 1 - ep01/08

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Layout from './Components/layout';
 
 function App() {
-  return <div className="App">gitHub api</div>
+  return <main className="App">
+    <Layout>
+      <div>
+        <img src="https://avatars.githubusercontent.com/u/47307816?v=4" alt="avatar"></img>
+        <h1>Luis Ricardo</h1>
+        <h3>Username:</h3>
+        <span>eleerry</span>
+        <div>
+          <h4>Followers</h4>
+          <span>5</span>
+        </div>
+        <div>
+          <h4>Starreds</h4>
+          <span>5</span>
+        </div>
+        <div>
+          <h4>Followings</h4>
+          <span>5</span>
+        </div>
+      </div>
+      <div>Repositories</div>
+      <div>Starreds</div>
+    </Layout>
+  </main>
 }
 
 export default App;
---
+++ src/Components/layout/index.js
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+
+function Layout({children}){
+    return(
+        <section>
+            <header>Header</header>
+            {children}
+        </section>
+    )
+
+}
+
+export default Layout
